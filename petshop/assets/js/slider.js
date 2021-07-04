0e90ba1fccdad04f8b4ee7573fea82d3c84f4338
kh-almas <services.almas@gmail.com>
2021-07-05 01:22:54 +0600

    .......

--- petshop/assets/js/slider.js
+++ petshop/assets/js/slider.js
@@ -24,6 +24,67 @@ $(document).ready(function(){
         }]
     });
 
+    //single product
+    $("#single_product_slider").slick({
+        // normal options...
+        infinite: true,
+        dots: true,
+        autoplay:true,
+        pauseOnHover:false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplaySpeed: 4000,
+        arrows: false,
+        centerMode: false,
+        variableWidth: false,
+
+
+        // the magic
+        responsive: [{
+
+            breakpoint: 300,
+            settings: "unslick" // destroys slick
+
+        }]
+    });
+
+
+
+    // sliderexample
+
+    $("#sliderexample").slick({
+        // normal options...
+        slidesToShow: 1,
+
+
+        // the magic
+        responsive: [{
+
+            breakpoint: 300,
+            settings: "unslick" // destroys slick
+
+        }]
+    });
+
+
+    $('#external-buttons a').on('click', function(e){
+        e.preventDefault();
+        $('#external-buttons a.active').removeClass('active');
+        $(this).addClass('active');
+        var targetSlide = $(this).data('target');
+        $('.slick-slider').slick('slickGoTo', targetSlide );
+    });//click()
+
+
+
+
+
+
+
+
+
+
+
     //dog-training
     $("#paket_training_slider").slick({
         // normal options...
@@ -146,6 +207,102 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplaySpeed: 2000,
+        arrows: false,
+        centerMode: false,
+        variableWidth: false,
+
+
+        // the magic
+        responsive: [{
+
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                infinite: true
+            }
+
+        }, {
+
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                infinite: true
+            }
+
+        }, {
+
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                dots: true
+            }
+
+        }, {
+
+            breakpoint: 300,
+            settings: "unslick" // destroys slick
+
+        }]
+    });
+
+    //single_product
+    $("#same_product_slider").slick({
+        // normal options...
+        infinite: true,
+        dots: true,
+        autoplay:true,
+        pauseOnHover:false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplaySpeed: 3000,
+        arrows: false,
+        centerMode: false,
+        variableWidth: false,
+
+
+        // the magic
+        responsive: [{
+
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                infinite: true
+            }
+
+        }, {
+
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                infinite: true
+            }
+
+        }, {
+
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                dots: true
+            }
+
+        }, {
+
+            breakpoint: 300,
+            settings: "unslick" // destroys slick
+
+        }]
+    });
+
+
+    $("#same_shop_slider").slick({
+        // normal options...
+        infinite: true,
+        dots: true,
+        autoplay:true,
+        pauseOnHover:false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplaySpeed: 3000,
         arrows: false,
         centerMode: false,
         variableWidth: false,
--- petshop/assets/js/style.js
+++ petshop/assets/js/style.js
@@ -4,3 +4,12 @@ $(document).ready(function(){
 
     });
 });
+
+//pet shop start
+function show_shop_sidebar() {
+    document.getElementById('shop_sidebar').classList.remove("d-none");
+}
+function hide_shop_sidebar() {
+    document.getElementById('shop_sidebar').classList.add('d-none','d-md-block');
+}
+//pet shop end
